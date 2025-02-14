c9ed3b0ca0e0cb85d91368ffbad7a140ab2d9ae4
RafaFigueiroa <rafatinsfig@gmail.com>
2025-02-14 09:19:55 -0300

    modulazing ui components

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -2,57 +2,57 @@ import Image from "next/image";
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col items-center justify-center relative bg-primaria text-white">
-            <Image src={"/onda-rodape.svg"} alt="" width={1900} height={18.3} className="bg-primaria"/>
+        <footer className="flex flex-col items-center justify-center text-white text-sm -pt-2 overflow-hidden w-full bg-primaria -mt-[3px]">
+            <div className="w-full h-[18.27px] bg-[url(/footer/onda-rodape.svg)] bg-center"></div>
 
-            <div className="bg-primaria w-full px-8 pt-10 pb-[30px]">
-            <div className="flex w-full justify-between items-center pb-10">
-                <a href="#" className="w-72">
-                <Image src={"/logo-salinas-maragogi.svg"} alt="" width={169.3} height={72}/>
-                </a>
+            <div className="w-full px-8 pt-10 pb-3 bg-primaria">
+                <div className="flex w-full justify-between items-center pb-2">
+                    <a href="#" className="w-72">
+                        <Image src={"/footer/logo-salinas-maragogi.svg"} alt="" width={112.9} height={48}/>
+                    </a>
 
-                <button className="bg-secundaria rounded-xl text-[30px] font-semibold px-12 py-4">VEJA OUTROS PERÍODOS</button>
+                    <button className="bg-secundaria rounded-xl text-2xl font-semibold px-12 py-3 duration-300 hover:scale-110">VEJA OUTROS PERÍODOS</button>
 
-                <div className="flex gap-5">
-                <a href="https://web.facebook.com/salinasmaragogi/?locale=pt_BR&_rdc=1&_rdr#" target="_blank">
-                    <Image src={"/facebook.svg"} alt="" width={38} height={38}/>
-                </a>
-                <a href="https://www.instagram.com/salinasmaragogi/" target="_blank">
-                    <Image src={"/instagram.svg"} alt="" width={37} height={38}/>
-                </a>
-                <a href="https://www.youtube.com/salinasmaragogi" target="_blank">
-                    <Image src={"/youtube.svg"} alt="" width={53.6} height={38}/>
-                </a>
-                <a href="https://open.spotify.com/user/wi074zf7qkrgob3o3mkhiyxq6?si=66448c8c965648f6" target="_blank">
-                    <Image src={"/spotify.svg"} alt="" width={38} height={38}/>
-                </a>
-                <a href="https://www.tiktok.com/@salinasmaragogi?lang=en" target="_blank">
-                    <Image src={"/tiktok.svg"} alt="" width={47.5} height={38}/>
-                </a>
+                    <div className="flex gap-5">
+                        <a href="https://web.facebook.com/salinasmaragogi/?locale=pt_BR&_rdc=1&_rdr#" target="_blank">
+                            <Image src={"/footer/facebook.svg"} alt="" width={38} height={38}/>
+                        </a>
+                        <a href="https://www.instagram.com/salinasmaragogi/" target="_blank">
+                            <Image src={"/footer/instagram.svg"} alt="" width={37} height={38}/>
+                        </a>
+                        <a href="https://www.youtube.com/salinasmaragogi" target="_blank">
+                            <Image src={"/footer/youtube.svg"} alt="" width={53.6} height={38}/>
+                        </a>
+                        <a href="https://open.spotify.com/user/wi074zf7qkrgob3o3mkhiyxq6?si=66448c8c965648f6" target="_blank">
+                            <Image src={"/footer/spotify.svg"} alt="" width={38} height={38}/>
+                        </a>
+                        <a href="https://www.tiktok.com/@salinasmaragogi?lang=en" target="_blank">
+                            <Image src={"/footer/tiktok.svg"} alt="" width={47.5} height={38}/>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <hr className="bg-[#FFD4AB]"/>
+                <div>
+                    <hr className="bg-[#FFD4AB]"/>
 
-                <ul className="py-[30px] flex flex-col gap-3">
-                <li>*Criança de 0 a 12 anos de idade, hospedada com dois adultos pagantes na mesma acomodação, conforme capacidade da categoria. A criança deverá ter até 12 anos de idade no momento da hospedagem.</li>
-                <li>**Cancelamento grátis até 30 dias antes do check-in. Em caso de cancelamento após este prazo, será cobrada uma multa no valor correspondente a primeira diária. Em caso de no show, será cobrado o valor total da reserva.</li>
-                <li>Valor exibido é referente ao apartamento Standard Duplo já calculado o desconto de 5% do programa Tô de Volta, disponível após login no processo de compra.</li>
-                <li>No ato da reserva será cobrado do seu cartão de crédito o valor total da hospedagem podendo ser dividido em até 10x.</li>
-                <li>Você pode encontrar mínimo de noites e preços diferentes em alguns períodos</li>
-                </ul>
+                    <ul className="py-2 flex flex-col gap-3">
+                        <li>*Criança de 0 a 12 anos de idade, hospedada com dois adultos pagantes na mesma acomodação, conforme capacidade da categoria. A criança deverá ter até 12 anos de idade no momento da hospedagem.</li>
+                        <li>**Cancelamento grátis até 30 dias antes do check-in. Em caso de cancelamento após este prazo, será cobrada uma multa no valor correspondente a primeira diária. Em caso de no show, será cobrado o valor total da reserva.</li>
+                        <li>Valor exibido é referente ao apartamento Standard Duplo já calculado o desconto de 5% do programa Tô de Volta, disponível após login no processo de compra.</li>
+                        <li>No ato da reserva será cobrado do seu cartão de crédito o valor total da hospedagem podendo ser dividido em até 10x.</li>
+                        <li>Você pode encontrar mínimo de noites e preços diferentes em alguns períodos</li>
+                    </ul>
 
-                <hr />
+                    <hr />
 
-                <div className="py-7 flex gap-8 justify-center">
-                <p>Política de privacidade</p>
-                <p>Copyright 2024 © Grupo Amarante. Todos os direitos reservados.</p>
+                    <div className="pt-2 flex gap-8 justify-center">
+                        <p>Política de privacidade</p>
+                        <p>Copyright 2024 © Grupo Amarante. Todos os direitos reservados.</p>
+                    </div>
+
+                    <div className="flex justify-center">
+                        <Image src={"/footer/amarante.svg"} alt="" width={106} height={40}/>
+                    </div>
                 </div>
-
-                <div className="flex justify-center">
-                <Image src={"/amarante.svg"} alt="" width={212} height={80}/>
-                </div>
-            </div>
             </div>
 
         </footer>
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,13 +1,41 @@
+"use client";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 
 export default function Header() {
+  const pathname = usePathname();
+
+  if (pathname.endsWith("/")) {
     return (
-      <header className="bg-[url(../public/topo-1.png)] bg-no-repeat bg-center bg-cover w-full h-[740px] flex justify-center pt-4 relative">
-        <div>
-          <Image src="/ferias-julho-2024.png" alt="Férias de Julho 2024 - Salinas Maragogi" width={787} height={350}/>
-          <Image src="/desconto.png" alt="15% de desconto" width={232} height={106} className="rotate-1 absolute right-1/4 top-80"/>
-          <Image src="/icones1.svg" alt="" width={1152} height={218} className="absolute left-96 bottom-14"/>
+      <header className="bg-[url(/header/topo-maragogi.png)] bg-cover bg-bottom w-full h-[600px] overflow-hidden">
+        <div className="flex flex-col items-center relative pt-4 space-y-36">
+          <Image src="/header/ferias-julho-maragogi.svg" alt="Férias de Julho 2024 - Salinas Maragogi" width={524.6} height={233.3}/>
+          <Image src="/header/desconto-maragogi.svg" alt="15% de desconto" width={155} height={71} className="rotate-1 absolute left-2/3 top-24"/>
+
+          <div className="flex justify-around w-[1552px]">
+            <Image src="/header/icones-maragogi1.svg" alt="" width={150} height={75}/>
+            <Image src="/header/icones-maragogi1.svg" alt="" width={150} height={75} className="rotate-180"/>
+          </div>
         </div>
       </header>
     );
+  } else if (pathname.endsWith("/maceio")) {
+    return (
+      <header className="bg-[url(/header/topo-maceio.png)] bg-cover bg-bottom min-w-[1900px] h-[600px] overflow-hidden">
+        <div className="flex flex-col items-center relative pt-4 space-y-36">
+          <Image src="/header/ferias-julho-maceio.svg" alt="Férias de Julho 2024 - Salinas Maceio" width={524.6} height={233.3}/>
+          <Image src="/header/desconto-maceio.svg" alt="15% de desconto" width={155} height={71} className="rotate-1 absolute left-2/3 top-24"/>
+
+          <div className="flex justify-around w-[1552px]">
+            <Image src="/header/icones-maceio1.svg" alt="" width={150} height={75}/>
+            <Image src="/header/icones-maceio1.svg" alt="" width={150} height={75} className="rotate-180"/>
+          </div>
+        </div>
+      </header>
+    );
+  } else {
+    return (
+      <>Erro!</>
+    );
+  }
 }
